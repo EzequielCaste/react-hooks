705bfd2ea9e66e36be0d6d589c26aae1f0da18fd
EzequielCaste <ezequiel.caste@gmail.com>
2021-09-13 11:06:19 -0300

    product cart with useReducer

--- usereducer-app/src/App.js
+++ usereducer-app/src/App.js
@@ -1,11 +1,13 @@
 import CounterApp from "./CounterApp";
+import ProductApp from "./ProductApp";
 import TodoApp from "./TodoApp";
 
 function App() {
   return (
     <>
       {/* <CounterApp /> */}
-      <TodoApp />
+      {/* <TodoApp /> */}
+      <ProductApp />
     </>
   );
 }

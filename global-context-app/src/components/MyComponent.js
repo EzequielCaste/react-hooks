f1f544929f3e3afef66164d38f09b6d4403df6f5
EzequielCaste <ezequiel.caste@gmail.com>
2021-09-15 19:01:19 -0300

    Global context with custom hooks

---
+++ global-context-app/src/components/MyComponent.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useDispatch, useStore } from '../store/StoreProvider';
+import { types } from '../store/storeReducer';
+
+const MyComponent = () => {
+  const {user, products} = useStore();
+  const dispatch = useDispatch();  
+
+  return (
+    <div>
+      <h2>My Component</h2>
+      <h2>User: {user?.name}</h2>
+      <button onClick={() => dispatch({type: types.authLogout})}>
+        Logout
+      </button>
+      <button onClick={() => dispatch({
+        type: types.authLogin,
+        payload: { id: 1, name: 'Ezequiel'}
+      })}>
+        Login
+      </button>
+
+      <h2>Products</h2>
+      <ul>
+        {
+          products.map(product => (
+            <li key={product.id}>{product.title}</li>
+          ))
+        }
+      </ul>
+      <button onClick={() => dispatch({ type: types.productDeleteAll})}>
+        Delete All
+      </button>
+      <button onClick={() => dispatch({ type: types.productChange})}>
+        Change
+      </button>
+    </div>
+  )
+}
+
+export default MyComponent
